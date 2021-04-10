585d2e4baf8a4aaf9ba7923db9d6ab0fd070f8cd
Victoria293 <80681498+Victoria293@users.noreply.github.com>
2021-04-10 20:06:55 +0200

    Update Robotic_Arm_GR.ino

--- Robotic_Arm_GR.ino
+++ Robotic_Arm_GR.ino
@@ -1,10 +1,6 @@
 #include "MeMegaPi.h"
 #include <Servo.h>
 
-//el pepe
-
-// ...
-
 MePort limitSwitch(PORT_7);
 Servo svs[1] = {Servo()};
 MeStepperOnBoard steppers[3] = {MeStepperOnBoard(PORT_1),MeStepperOnBoard(PORT_2),MeStepperOnBoard(PORT_3)}; 
